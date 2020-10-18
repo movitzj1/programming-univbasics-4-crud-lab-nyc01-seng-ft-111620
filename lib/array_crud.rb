0850ba088ca13261bd071ef747e85e69bf0850dd
Justin Movitz <movitzj1@montclair.edu>
2020-10-18 16:40:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  ["HZ2000" "A8H", "CX", "Z9D"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  
 end
 
 def add_element_to_start_of_array(array, element)
