bef8acc7a893a917534c084a419b75a2ffe10397
Justin Movitz <movitzj1@montclair.edu>
2020-10-18 16:59:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,27 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "X940E"
+  new_lst_1 = array << "X940E"
+  return new_lst_1
 end
 
 def add_element_to_start_of_array(array, element)
-
+  new_lst_2 = array.upshift("P50-C1")
+  return new_lst_2
 end
 
 def remove_element_from_end_of_array(array)
-
+  new_lst_3 = array.pop()
+  return new_lst_3
 end
 
 def remove_element_from_start_of_array(array)
-
+  new_lst_4 = array.shift
+  return new_lst_4
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
